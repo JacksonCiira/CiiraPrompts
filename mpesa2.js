7c4f41d223b2bc3c589a2fb7bd36a22f9f46fb95
Jackson Ciira <chiraciira0@gmail.com>
2025-03-17 10:47:45 +0300

    Delete mpesa2.js

--- mpesa2.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")
-
-const task = prompt("Do you want to send or withdraw? ")
-const taskMessage = task === "send"
-if (taskMessage){
-    console.log("Enter Amount...  $")
-} else {
-    console.log("Withdraw money")
-}
